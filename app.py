321d395260524c98aeeb5d767d259a24680f5a9e
Omkar Jagtap <omkarjagtap1011@gmail.com>
2026-01-02 13:50:59 +0530

    solved config error

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def setup_rclone():
     # 3. Path to save the config (Linux /tmp is best for Streamlit Cloud)
     config_path = "/tmp/rclone.conf"
     
-    with open(config_path, "w") as f:
+    with open(config_path, "wb") as f:
         f.write(decoded_conf)
     
     return config_path
